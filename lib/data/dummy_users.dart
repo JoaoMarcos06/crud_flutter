67b2060e66276c5dd54f8745d9710fa2ca78fabe
João Marcos <joao.marcos@inovarymktdigital.com.br>
2020-12-14 22:35:54 -0300

    CRUD em Flutter

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/User.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708__340.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png')
+};
